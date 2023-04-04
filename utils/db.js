de97cdc3383be6f56e5c18612db7192788f61259
tobexint <tobexint@gmail.com>
2023-04-03 21:46:38 -0700

    Working with mongodb client

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(this.url, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const myDb = this.client.db();
+    return myDb.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    const myDb = this.client.db();
+    return myDb.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
