a32a286937844b4aaab328e70495bd72e2fc3a30
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-04-05 19:21:24 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 /* Routes */
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
