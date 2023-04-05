768158cca0c1ba79ab4805215665f94f5b4d41b2
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-04-05 19:49:35 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 /* Routes */
 import express from 'express';
 import AppController from '../controllers/AppController';
-import Users from '../controllers/UsersController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-router.post('/users', Users.postNew);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
