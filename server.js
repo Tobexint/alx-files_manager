bf7b318b51d4133d89e198d6f5c6c208e71045fc
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-04-05 18:04:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(router);
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
