bbd69263384443dfcc831943be13ab9c6108afd0
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-04-06 09:24:27 +0000

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,7 +2,7 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 import { ObjectId } from 'mongodb';
-import sha1 from 'sha1';
+import { createHash } from 'crypto';
 
 class UsersController {
   static async postNew(req, res) {
@@ -34,5 +34,38 @@ class UsersController {
       email,
     });
   }
+
+  static async getMe(req, res) {
+    const authToken = req.header('X-Token') || null;
+    if (!authToken) {
+      res.status(401).send({
+        error: 'Unauthorized',
+      });
+      return;
+    }
+    const token = `auth_${authToken}`;
+    const user = await redisClient.get(token);
+    if (!user) {
+      res.status(401).send({
+        error: 'Unauthorized',
+      });
+      return;
+    }
+    const users = dbClient.client.db().collection('users');
+    const userData = await users.findOne({
+      _id: ObjectId(user),
+    });
+    if (userData) {
+      res.status(200).send({
+        id: user,
+        email: userData.email,
+      });
+    } else {
+      res.status(401).send({
+        error: 'Unauthorized',
+      });
+    }
+  }
+} 
 
 module.exports = UsersController;
